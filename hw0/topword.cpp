02baa44972c2f7d2e8599ac0f1003578b59fdd5a
cod1r <jasonho353@gmail.com>
2022-08-28 18:03:09 -0500

    fixed a couple of bugs and incorporated a better understanding of what the assignment requires

--- hw0/topword.cpp
+++ hw0/topword.cpp
@@ -4,7 +4,7 @@
 #include <map>
 #include <vector>
 #include <algorithm>
-#include <string.h>
+#include <queue>
 
 int main(int argc, char *argv[])
 {
@@ -34,26 +34,11 @@ int main(int argc, char *argv[])
 		input_file_stream >> line;
 		if (line[0] != '\0') {
 			{
-				// this scope checks to see if numbers and letters are mixed
-				bool has_alpha = false;
-				bool has_digit = false;
-				for (int i = 0; i < line.length(); ++i) {
-					if (line[i] >= 97 && line[i] <= 122)
-						has_alpha = true;
-					else if (line[i] >= 48 && line[i] <= 57) {
-						has_digit = true;
-					}
-				}
-				if (has_alpha && has_digit)
-					continue;
-			}
-
-			{
 				// this scope turns the string to lowercase
 				for (char &c : line)
 					c = std::tolower(c);
 			}
-
+			std::vector<std::string> words;
 			{
 				// this checks to see if there are any strings that are separated by punctuation (any character that isn't a number or letter)
 				// and counts that
@@ -62,31 +47,58 @@ int main(int argc, char *argv[])
 				for (int i = 0; i < line.length(); ++i) {
 					if (line[i] < 48 || line[i] > 122) {
 						split = true;
-						count[line.substr(last, i - last)] += 1;
-						while (line[i] < 48 || line[i] > 122) ++i;
+						words.push_back(line.substr(last, i - last));
+						while (i < line.length() && (line[i] < 48 || line[i] > 122)) ++i;
 						last = i;
 					}
-					if (i == line.length() - 1) {
-						count[line.substr(last)] += 1;
+					if (i == line.length() - 1 && last < line.length()) {
+						words.push_back(line.substr(last));
 					}
 				}
-				if (!split)
-					count[line] += 1;
 			}
+
+			// this checks to see if any numbers are present, if so then that means we ignore
+			for (std::string s : words) {
+				bool has_digit = false;
+				for (int i = 0; i < s.length(); ++i) {
+					if (s[i] >= 48 && s[i] <= 57) {
+						has_digit = true;
+					}
+				}
+				if (has_digit) {
+					continue;
+				}
+				count[s] += 1;
+			}
+
+
 		}
 	}
 	std::ofstream output_file_stream(output_file);
-	// vector so that can sort the words in O(nlogn) time
-	std::vector<std::pair<std::string, int>> words_sorted;
-	// puts words in vector so we can sort
+	// using a priority queue so that every time we push an <int, string> pair, it takes log n time which
+	// ends up being nlogn for n pairs.
+
+	// In C++, priority_queues by default sorts by descending order and pairs are compared by their first element,
+	// which means that words with a higher count will be higher in the priority queue which then means that they will come out first.
+	std::priority_queue<std::pair<int, std::string>> q;
+	// pushing onto priority_queue
 	for (auto p : count) {
-		words_sorted.push_back(std::pair<std::string, int>{p.first, p.second});
+		q.push(std::pair<int, std::string>{p.second, p.first});
 	}
-	// sorts the words alphabetically
-	std::sort(words_sorted.begin(), words_sorted.end());
-	for (auto p : words_sorted) {
-		output_file_stream << p.first << ' ' << p.second << '\n';
+	// we keep track of the greatest_count so that we can print out all of the tied "most frequent word".
+	int greatest_count = q.size() > 0 ? q.top().first : 0;
+	// vector that holds all of the most frequent tied words and sorted them alphabetically
+	std::vector<std::pair<std::string, int>> top_words_sorted;
+	while (q.size() > 0 && q.top().first == greatest_count) {
+		top_words_sorted.push_back(std::pair<std::string, int>{q.top().second, q.top().first});
+		q.pop();
 	}
+	// C++ uses "introsort" which is a combination of sorts that are used depending on the size of the input.
+	std::sort(top_words_sorted.begin(), top_words_sorted.end());
+	for (auto p : top_words_sorted) {
+		output_file_stream << p.first << ' ' << p.second  << '\n';
+	}
+	// we close the file streams after we are done with them.
 	input_file_stream.close();
 	output_file_stream.close();
 	return 0;
